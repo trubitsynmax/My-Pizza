4501f8bf6b54d61f744ae9649601f31257b8ad91
Maxim <trubitsynmaxaug@gmail.com>
2024-12-21 18:46:09 +0300

    Связал проект с маленьким сервером
    * В качестве сервера использоал mocapi
    * Отправил на сервер данные о пиццах и их характеристика
    * Сделал запрос fetch и использованием хука useEffect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./scss/app.scss";
 import { Header, Sort } from "./components/script";
 import { Categories } from "./components/script";
 import { Card } from "./components/script";
-import { data } from "./assets/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    fetch("https://6766d6fd560fbd14f18c43df.mockapi.io/MyPizza")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((e) => setError(true));
+  }, []);
   return (
     <div className="wrapper" onClick={isOpen ? () => setIsOpen(false) : null}>
       <Header />
@@ -19,9 +25,14 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {data.map((item) => (
-              <Card {...item} key={item.id} />
-            ))}
+            {error ? (
+              <div>
+                К сожалению, произошла какая ошибка, попробуйте повторить
+                попытку позже 😟
+              </div>
+            ) : (
+              data.map((item) => <Card {...item} key={item.id} />)
+            )}
           </div>
         </div>
       </div>
