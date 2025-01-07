c0c8095c9b1f77ac114b4561a201e719a2c3e3e0
Maxim <trubitsynmaxaug@gmail.com>
2025-01-07 17:21:13 +0300

    Оптимизация запросов и поиска
    * Сделал имитацию debounce, установив функцию из библиотеки lodash
    * Заменил fetch запрос на axios
    * Исправил незначительный баг в css, на мобилке конпка корзины перекрывала не весь контент
    * Использовал хук useRef для того, чтобы получить input, это нужно было для того, чтобы при нажатии на крестик фокус с инпута не пропадал
    * Использовал хук useCallback для того, чтобы debounce отрисовывался всего-лишь один раз и работал корректно

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,22 +1,42 @@
-import { useContext } from "react";
+import { useCallback, useContext, useRef, useState } from "react";
 import styles from "./Input.module.scss";
 import { searchContext } from "../../App";
+import debounce from "lodash.debounce";
 
 export default function Input() {
+  const [value, setValue] = useState("");
   const { usersValue, setUsersValue } = useContext(searchContext);
+  const inputRef = useRef();
+
+  const focusInput = () => {
+    setValue("");
+    setUsersValue("");
+    inputRef.current.focus();
+  };
+
+  const getInput = useCallback(
+    debounce((str) => {
+      setUsersValue(str);
+    }, 1000),
+    []
+  );
+
+  const eventInput = (event) => {
+    setValue(event.target.value);
+    getInput(event.target.value);
+  };
   return (
     <div className={styles.root}>
       <input
-        value={usersValue}
-        onChange={(event) => setUsersValue(event.target.value)}
+        ref={inputRef}
+        value={value}
+        onChange={eventInput}
         type="text"
         placeholder="Поиск пицц"
         className={styles.input}
         maxLength={45}
       />
-      {usersValue && (
-        <div className={styles.cross} onClick={() => setUsersValue("")}></div>
-      )}
+      {value && <div className={styles.cross} onClick={focusInput}></div>}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Card, Categories, Skeleton, Sort } from "../components/script";
 import { searchContext } from "../App";
 import { useSelector, useDispatch } from "react-redux";
 import { setFilter, setSort } from "../redux/slices/filterSlice";
+import axios from "axios";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -21,20 +22,27 @@ export default function Home() {
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [MoreOrLess, setMoreOrLess] = useState(true);
+
   const isCategory = filter > 0 ? `category=${filter}` : "";
   const isFilter = MoreOrLess ? "asc" : "desc";
   const isSort = `sortBy=${sort.sortProperty}`;
+  const inputFilter = `filter=${usersValue}`;
+
   const isCart = useEffect(() => {
     setIsLoading(true);
-    fetch(
-      `https://6766d6fd560fbd14f18c43df.mockapi.io/MyPizza?${isCategory}&${isSort}&order=${isFilter}`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json);
+    setError(false);
+    axios
+      .get(
+        `https://6766d6fd560fbd14f18c43df.mockapi.io/MyPizza?${isCategory}&${isSort}&order=${isFilter}`
+      )
+      .then((res) => {
+        setData(res.data);
         setIsLoading(false);
       })
-      .catch((e) => setError(true));
+      .catch(() => {
+        setError(true);
+        setIsLoading(false);
+      });
   }, [filter, sort.sortProperty, MoreOrLess]);
 
   const skeleton = [...new Array(6)].map((_, index) => (
@@ -49,7 +57,7 @@ export default function Home() {
       return false;
     })
     .map((item, index) => <Card key={index} {...item} />);
-
+  console.log("list", listPizza.length > 0, "error", error);
   return (
     <>
       <div className="container">
@@ -70,12 +78,13 @@ export default function Home() {
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
-          {error && (
-            <div>
-              К сожалению, произошла какая ошибка, попробуйте повторить попытку
-              позже 😟
-            </div>
-          )}
+          {error ||
+            (listPizza.length === 0 && (
+              <div>
+                К сожалению, произошла какая ошибка или такой пиццы не
+                существует, попробуйте повторить попытку позже 😟
+              </div>
+            ))}
           {isLoading ? skeleton : listPizza}
         </div>
       </div>
